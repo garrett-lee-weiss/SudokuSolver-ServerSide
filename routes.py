b1528704ceda4b9aca577bac07dd0fda20720a7b
Garrett Weiss <gweiss@acustream.com>
2020-07-26 20:28:04 -0600

    additional refactoring, removed self.value_list to reduce redundancy with self.value_dictionary

--- routes.py
+++ routes.py
@@ -7,9 +7,9 @@ empty = [[''] * 9 for i in range(9)]
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        return render_template('frontend.html', matrix = empty)
+        return render_template('frontend.html', matrix=empty)
     if request.method == 'POST':
         puzzle_input = dict(request.form)
         solve_me = SolvePuzzle(puzzle_input)
         solution = solve_me.final_solved_puzzle
-        return render_template('frontend.html', solved = solve_me.is_puzzle_solved, matrix = solution)
+        return render_template('frontend.html', solved=solve_me.is_puzzle_solved, matrix=solution)
--- solver.py
+++ solver.py
@@ -1,29 +1,30 @@
 import pandas as pd
 
+
 class SolvePuzzle:
 
-    def __init__(self, dict, inception=False):
+    def __init__(self, dict_of_user_inputs, inception=False):
 
         if inception:
-            self.current_puzz_value_dict = dict
+            self.current_puzz_values_dict = dict_of_user_inputs
         else:
-            self.current_puzz_value_dict = self.create_clean_dict(dict)
+            self.current_puzz_values_dict = self.create_clean_dict(dict_of_user_inputs)
 
         self.final_solved_puzzle = None
-        self.current_puzz_data_frame, self.current_puzz_value_list = self.convert_to_data_frame()
+        self.current_puzz_data_frame = self.convert_to_data_frame()
         self.row_sets, self.col_sets, self.box_sets = self.create_sets()
         self.count_solutions_by_cell, self.potential_solutions_by_cell = self.find_potential_solutions()
         self.is_puzzle_solved = self.solve()
 
-    def create_clean_dict(self, data_from_html):
+    def create_clean_dict(self, dict_of_user_inputs):
 
         # converts dictionary of strings '(x, y)':'#' to tuple(x,y): int(#)
 
         clean_dict = {}
-        for coordinate in data_from_html:
+        for coordinate in dict_of_user_inputs:
             new_coordinate, new_value = tuple(int(coordinate[i]) for i in (1,4)), None
-            if data_from_html[coordinate].isdigit():
-                new_value = int(data_from_html[coordinate])
+            if dict_of_user_inputs[coordinate].isdigit():
+                new_value = int(dict_of_user_inputs[coordinate])
             clean_dict[new_coordinate] = new_value
         return clean_dict
 
@@ -32,16 +33,16 @@ class SolvePuzzle:
         # Creates data frame from current puzzle values. Data frame is used in creating sets.
 
         list = [[None] * 9 for i in range(9)]
-        for key in self.current_puzz_value_dict:
-            list[key[0]][key[1]] = self.current_puzz_value_dict[key]
-        return pd.DataFrame(list), list
+        for key in self.current_puzz_values_dict:
+            list[key[0]][key[1]] = self.current_puzz_values_dict[key]
+        return pd.DataFrame(list)
 
     def create_sets(self):
 
         # creates sets of values from current row, column, and box entries in the puzzle. Discards empty strings
 
         self.current_puzz_data_frame.fillna('', inplace=True)
-        row_dict = {f'row_{i}' : set(self.current_puzz_data_frame.loc[i]) for i in range(9)}
+        row_dict = {f'row_{i}': set(self.current_puzz_data_frame.loc[i]) for i in range(9)}
         col_dict = {f'col_{i}': set(self.current_puzz_data_frame[i]) for i in range(9)}
         box_dict = {'box_0': self.convert_to_set(self.current_puzz_data_frame.loc[0:2, 0:2]),
                     'box_1': self.convert_to_set(self.current_puzz_data_frame.loc[0:2, 3:5]),
@@ -61,26 +62,26 @@ class SolvePuzzle:
         return row_dict, col_dict, box_dict
 
     def assign_new_values(self,row, column, value):
-        self.current_puzz_value_list[row][column] = value
-        self.current_puzz_value_dict[(row, column)] = value
-        self.current_puzz_data_frame = pd.DataFrame(self.current_puzz_value_list)
+        self.current_puzz_values_dict[(row, column)] = value
+        self.current_puzz_data_frame = self.convert_to_data_frame()
         self.row_sets, self.col_sets, self.box_sets = self.create_sets()
         self.count_solutions_by_cell, self.potential_solutions_by_cell = self.find_potential_solutions()
 
     def find_potential_solutions(self):
 
-        # finds differences in every row, column, and box set compared to the complete set to find values missing from the set.
+        # finds differences in every row, column, and box set compared to the complete set.
         # Differences are overlapped to determine potential values for every cell.
+        # Only cells without existing values are assigned.
 
         complete_set = {i for i in range(1, 10)}
         count_dict, value_dict = {}, {}
-        for row in range(0,9):
-            for column in range(0,9):
+        for row in range(0, 9):
+            for column in range(0, 9):
                 row_diff = complete_set.difference(self.row_sets[f'row_{row}'])
                 col_diff = complete_set.difference(self.col_sets[f'col_{column}'])
                 box_diff = complete_set.difference(self.box_sets[self.get_box_set(row, column)])
                 overlap = row_diff.intersection(col_diff.intersection(box_diff))
-                if not self.current_puzz_value_list[row][column]:
+                if self.current_puzz_values_dict[(row, column)] is None:
                     value_dict[(row, column)] = overlap
                     count_dict[(row, column)] = len(overlap)
 
@@ -94,7 +95,7 @@ class SolvePuzzle:
         # If the minimum num of solutions in all cells is two, plug the value into a dictionary( copied from current value dict)
         # and instantiate a new SolvePuzzle class with that dictionary.
 
-        solved= False
+        solved = False
         while not solved:
             small = min(set([self.count_solutions_by_cell[i] for i in self.count_solutions_by_cell]))
             if small == 0:
@@ -102,9 +103,9 @@ class SolvePuzzle:
             elif small == 1:
                 self.single_solution()
             else:
-                return self.try_inception()
+                return self.recursive_solution()
             solved = self.is_puzzle_solved()
-        self.final_solved_puzzle = self.current_puzz_value_list
+        self.final_solved_puzzle = self.current_puzz_values_dict
         return solved
 
     def single_solution(self):
@@ -112,12 +113,12 @@ class SolvePuzzle:
             if self.count_solutions_by_cell[item] == 1:
                 self.assign_new_values(item[0], item[1], self.potential_solutions_by_cell[item].pop())
 
-    def try_inception(self):
+    def recursive_solution(self):
         for item in self.count_solutions_by_cell:
             if self.count_solutions_by_cell[item] == 2:
                 value1, value2 = self.potential_solutions_by_cell[item].pop(), self.potential_solutions_by_cell[item].pop()
                 for i in (value1, value2):
-                    puzzle_copy_dict = self.current_puzz_value_dict.copy()
+                    puzzle_copy_dict = self.current_puzz_values_dict.copy()
                     puzzle_copy_dict[item] = i
                     trial = SolvePuzzle(puzzle_copy_dict, inception=True)
                     if trial.is_puzzle_solved:
@@ -126,15 +127,15 @@ class SolvePuzzle:
                 return False
 
     def is_puzzle_solved(self):
-        for row in range(0,9):
-            for column in range(0,9):
-                if self.current_puzz_value_list[row][column] is None:
+        for row in range(0, 9):
+            for column in range(0, 9):
+                if self.current_puzz_values_dict[(row, column)] is None:
                     return False
         return True
 
     @staticmethod
     def get_box_set(row, column):
-        list = [
+        box_list = [
             ['box_0', 'box_0', 'box_0', 'box_1', 'box_1', 'box_1', 'box_2', 'box_2', 'box_2'],
             ['box_0', 'box_0', 'box_0', 'box_1', 'box_1', 'box_1', 'box_2', 'box_2', 'box_2'],
             ['box_0', 'box_0', 'box_0', 'box_1', 'box_1', 'box_1', 'box_2', 'box_2', 'box_2'],
@@ -145,11 +146,11 @@ class SolvePuzzle:
             ['box_6', 'box_6', 'box_6', 'box_7', 'box_7', 'box_7', 'box_8', 'box_8', 'box_8'],
             ['box_6', 'box_6', 'box_6', 'box_7', 'box_7', 'box_7', 'box_8', 'box_8', 'box_8'],
         ]
-        return list[row][column]
+        return box_list[row][column]
 
     @staticmethod
-    def convert_to_set(array):
-        list = []
-        for i in array.values:
-            list.extend(i)
-        return set(list)
+    def convert_to_set(nested_list):
+        flat_list = []
+        for i in nested_list.values:
+            flat_list.extend(i)
+        return set(flat_list)
