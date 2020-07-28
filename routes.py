7a069c0d09d7902878bb738c01bd26c22c204999
Garrett Weiss <gweiss@acustream.com>
2020-07-27 20:38:14 -0600

    replacing old form submission with AJAX form, page no longer needs to refresh to solve the puzzle

--- routes.py
+++ routes.py
@@ -1,15 +1,18 @@
 from flask import render_template, request
+import json
+
 from app import app
 from solver import SolvePuzzle
 
-empty = [[''] * 9 for i in range(9)]
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        return render_template('frontend.html', matrix=empty)
+        return render_template('frontend.html')
     if request.method == 'POST':
         puzzle_input = dict(request.form)
         solve_me = SolvePuzzle(puzzle_input)
         solution = solve_me.final_solved_puzzle
-        return render_template('frontend.html', solved=solve_me.is_puzzle_solved, matrix=solution)
+        solution_with_strings = {str(key): str(value) for (key, value) in solution.items()}
+        return json.dumps(solution_with_strings)
+
--- static/functionalScripts.js
+++ static/functionalScripts.js
@@ -3,9 +3,32 @@ function submit() {
     }
 
 function clearInputs() {
-    var x = document.getElementsByTagName("input");
-    var i;
+    let x = document.getElementsByTagName("input");
+    let i;
     for (i = 0; i < 81; i++) {
-        x[i].value = ''
+        console.log(x[i]);
+        x[i].value = '';
         }
     }
+function sendData() {
+    let httpRequest = new XMLHttpRequest();
+    let form = document.getElementById("main_form");
+    let myForm = new FormData(form);
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+                fillInValues(httpRequest.response)
+                }
+            }
+    httpRequest.open('POST', '/');
+    httpRequest.send(myForm)
+    }
+
+function fillInValues(results) {
+    let obj = JSON.parse(results);
+    let j = document.getElementsByName("(7, 6)");
+    console.log(j.item(0));
+    for (var prop in obj) {
+        let j = document.getElementsByName(prop)[0];
+        j.value = obj[prop];
+    }
+}
